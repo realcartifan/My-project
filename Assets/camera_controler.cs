494fd94ca1473294a91455fba02e803f702d93f0
Dominik Żwirski <dominik.zwirski@teb.edu.pl>
2024-02-21 15:15:18 +0100

    nwe

---
+++ Assets/camera_controler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // pozycja gracza
+    Transform player;
+    // offset kamery
+    Vector3 cameraOffset;
+    //prêdkoœæ przesuwania kamery - nie nadajemy wartoœci - zrobi to smoothDamp
+    Vector3 cameraSpeed;
+    //czas wyg³adzania ruchu kamery
+    float smoothTime = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // znajdz gracza i pod³¹cz jego pozycjê do zmiennej transform
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        // pobierz domyœlny offset kamery
+        cameraOffset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //pozycja kamery to pozyja gracza + offset
+        //transform.position = player.position + cameraOffset;
+
+
+
+        //bardziej kulturalnie - lerp
+        //policz pozycje docelow¹
+        Vector3 targetPosition = player.position + cameraOffset;
+
+        //u¿yj interpolacji liniowej
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+
+        //u¿yj smooothdamp
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraSpeed, smoothTime);
+    }
+}
--- Assets/playercontroler.cs
+++ Assets/playercontroler.cs
@@ -1,42 +1,75 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercomponent : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    //silnik fizyczny dla obiektu gracza
+    Rigidbody rb;
+    //siła skoku
+    public float jumpForce = 5f;
+
     public float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //przypnij rigidbody gracza do zmiennej rb
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        //można prościej: Vector3.right
+
+        //zczytaj klawiaturę w osi poziomej:
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //wyświetl w konsoli stan klawiatury
+        Debug.Log(horizontalInput);
+
+        //wylicz przesunięcie w osi x
+        Vector3 movement = Vector3.right * horizontalInput;
+
+        //zczytaj z klawiatury oś y
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        //wylicz przesunięcie w osi y i dodaj do istniejącego przesunięcia w osi x
+        movement += Vector3.forward * verticalInput;
+
+        //normalizujemy wektor
+        movement = movement.normalized;
+        //poprawka na czas od ostatniej klatki
+        movement *= Time.deltaTime;
+        //pomnóż przez prędkość ruchu
+        movement *= moveSpeed;
+
+        //przesuń gracza w osi x
+        //transform.position += movement;
+
+        //próbujemy użyc translate zamiast dodawac współrzędne
+        transform.Translate(movement);
+
+
+    }
+    //spróbujmy obejść problem z opóźnieniem wejścia poprzez przeniesienie go do update
     void Update()
     {
-        //transform.position += Vector3.right * Time.deltaTime;
-        //zczytaj klawiature w osi poziomej
-        float x = Input.GetAxisRaw("Horizontal");
-
-        Debug.Log(x);   
-
-        Vector3 movment = Vector3.right * x ;
-
-        //transform.position += movment;
-
-        float y = Input.GetAxisRaw("Vertical");
-
-        movment += Vector3.forward * y ;
-
-        movment = movment.normalized;
-        movment *= Time.deltaTime;
-        movment *= moveSpeed; 
-
-        transform.position += movment;
-
-
-
-
+        //sprawdz czy nacisnieto spację (skok)
+        //zwraca true jeśli zaczęliśmy naciskać spację w trakcie klatki animacji
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        //sprawdz czy znajduje się na poziomie 0
+        if (transform.position.y <= Mathf.Epsilon)
+        {
+            //dodaj siłę skoku
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
 
     }
 }
